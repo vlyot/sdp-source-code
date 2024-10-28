17a3929350f9a665c073dbcb7e36c17d6ecf8064
Kai Chong Ng <ngkaichong10@gmail.com>
2024-10-28 16:26:29 +0800

    k

--- a1/obj/Debug/net8.0/a1.AssemblyInfo.cs
+++ a1/obj/Debug/net8.0/a1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("a1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3e89702af94ac8928f929ebd2a506b67ed946c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad2f3a49369b5c97c48eefa98194674bd12f0ac2")]
 [assembly: System.Reflection.AssemblyProductAttribute("a1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("a1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
